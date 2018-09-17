674ea341337fd866c5d474ed8a858276b0f46085
程林 <chenglin@chenglindeMacBook-Pro.local>
2018-09-17 17:23:54 +0800

    update

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -1,4 +1,4 @@
-import {shallow} from 'enzyme';
+import {shallow,mount} from 'enzyme';
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,31 +8,42 @@ Enzyme.configure({ adapter: new Adapter() });
 
 import {Form} from "antd/lib/index";
 const WrappedAdvancedFilter = Form.create()(Filter);
+const props = {
+    // Jest 提供的mock 函数
+    filterData:[{
+        id: 'activityCode',
+        name: '活动编码',
+        inputChangeName:'titleChangeFun'
+    },{
+        id: 'searchParam',
+        name: '查询条件',
+    }],
+    handleReset: jest.fn( (e) => {
+        console.log('reset','=============')
+    }),
+    handleSearch:jest.fn( (e) => {
+        console.log('search','=============')
+    }),
+    handleBack:jest.fn( (e) => {
+        console.log('back','=============')
+    }),
+}
 
-test('filter', () => {
-    const props = {
-        // Jest 提供的mock 函数
-        filterData:[{
-            id: 'activityCode',
-            name: '活动编码',
-            inputChangeName:'titleChangeFun'
-        },{
-            id: 'searchParam',
-            name: '查询条件',
-        }],
-        handleReset: jest.fn( (e) => {
-            console.log('reset','=============')
-        }),
-        handleSearch:jest.fn( (e) => {
-            console.log('search','=============')
-        }),
-        handleBack:jest.fn( (e) => {
-            console.log('back','=============')
-        }),
-    }
-    const filterWrapperTest = shallow(<div className={'test'} onClick={()=>{
-        console.log('test');
-    }}><WrappedAdvancedFilter {...props} ></WrappedAdvancedFilter></div>);
-    filterWrapperTest.find('div.test').simulate('click');
-    filterWrapperTest.find('button.reset').simulate('click');
+describe('filter', () => {
+    it('simulates click events', () => {
+        const component = renderer.create(
+            <div className={'test'} onClick={()=>{console.log('test')}}>
+                <WrappedAdvancedFilter {...props}/>
+            </div>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        const wrapper = shallow(
+            <div className={'test'} onClick={()=>{console.log('test')}}>
+                <WrappedAdvancedFilter {...props}/>
+            </div>);
+        // wrapper.find('div.test').simulate('click');
+        wrapper.find('button.reset').simulate('click');
+    });
 });
